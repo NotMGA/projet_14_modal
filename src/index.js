49e32214450df99ddc9a0ef75aef1bb463ab721f
NotMGA <maximefreyre@gmail.com>
2022-10-24 15:39:26 +0200

    v2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Close from './close.svg'
+function Modal(props) {
+  function action_close() {
+    const visibility = document.getElementsByClassName('modal_display')
+    visibility[0].style.display = 'none'
+    
+  }
+  return (
+    <div style={props.style_modal} className="modal_display">
+      <div style={props.style_txt}>{props.modal_txt}</div>
+      <button style={props.style_btn} onClick={action_close}>
+        <img style={props.style_img} src={Close} alt="croix"></img>
+      </button>
+    </div>
+  )
+}
+Modal.defaultProps = {
+  style_img: {
+    width: 'auto',
+    height: '20px',
+    border: '0px solid',
+    borderRadius: '205px',
+  },
+  style_modal: {
+    display: 'flex',
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    borderRadius: '205px',
+    border: '2px solid rgb(204, 51, 0)',
+    justifyContent: 'center',
+  },
+  style_btn: {
+    border: '0px solid',
+    padding: '0px',
+    right: ' -74px',
+    top: '-8px',
+    position: 'relative',
+    height: '20px',
+    borderRadius: '205px',
+  },
+  style_txt: { padding: '20px' },
+  modal_txt: 'Employee create',
+}
+export default Modal
